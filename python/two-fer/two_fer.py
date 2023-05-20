0970477131bbf0687df53a488e929740a1eaab7b
Davi Silveira <davilego10@gmail.com>
2023-05-20 16:34:17 -0300

    Exercício Two Fer adicionado

---
+++ python/two-fer/two_fer.py
@@ -0,0 +1,5 @@
+def two_fer(friend_name="you", likes_cookies=True):
+    if likes_cookies and friend_name == "Do-yun":
+        return f"One for {friend_name}, one for me."
+    else:
+        return f"One for {friend_name}, one for me."
